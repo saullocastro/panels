b60f63fbadfd09e19bb108342f8a4946b59588d3
saullocastro <castrosaullo@gmail.com>
2019-05-02 14:30:08 +0200

    TST: properly using structsolve

--- panels/tests/tests_shell/test_lb.py
+++ panels/tests/tests_shell/test_lb.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from structsolve import lb
 from panels import Shell
 from panels.plot_shell import plot_shell
 
@@ -20,21 +21,19 @@ def test_shell_lb():
         s.b = 1.
         s.r = 1.e8
         s.Nxx = -1
-        eigvals, eigvecs = s.lb()
+        eigvals, eigvecs = lb(s.calc_kC(), s.calc_kG(), silent=True)
         assert np.isclose(eigvals[0], 157.5369886, atol=0.1, rtol=0)
 
         s.Nxx = 0
         s.Nyy = -1
-        eigvals, eigvecs = s.lb()
+        eigvals, eigvecs = lb(s.calc_kC(), s.calc_kG(), silent=True)
         assert np.isclose(eigvals[0], 58.1488566, atol=0.1, rtol=0)
 
         # ssfs
         s = Shell()
-        s.u2ty = 1
-        s.v2ty = 1
-        s.w2ty = 1
-        s.u2ry = 1
-        s.v2ry = 1
+        s.y2u = 1
+        s.y2v = 1
+        s.y2w = 1
         s.m = 12
         s.n = 13
         s.stack = [0, 90, -45, +45]
@@ -45,23 +44,19 @@ def test_shell_lb():
         s.b = 0.5
         s.r = 1.e8
         s.Nxx = -1
-        eigvals, eigvecs = s.lb()
+        eigvals, eigvecs = lb(s.calc_kC(), s.calc_kG(), silent=True)
         assert np.isclose(eigvals[0], 15.8106238, atol=0.1, rtol=0)
 
-        s.u2tx = 1
-        s.v2tx = 1
-        s.w2tx = 1
-        s.u2rx = 1
-        s.v2rx = 1
-        s.u2ty = 0
-        s.v2ty = 0
-        s.w2ty = 0
-        s.u2ry = 0
-        s.v2ry = 0
+        s.x2u = 1
+        s.x2v = 1
+        s.x2w = 1
+        s.y2u = 0
+        s.y2v = 0
+        s.y2w = 0
         s.Nxx = 0
         s.Nyy = -1
-        eigvals, eigvecs = s.lb()
-        assert np.isclose(eigvals[0], 0.9706868, atol=0.1, rtol=0)
+        eigvals, eigvecs = lb(s.calc_kC(), s.calc_kG(), silent=True)
+        assert np.isclose(eigvals[0], 14.046, atol=0.1, rtol=0)
         plot_shell(s, eigvecs[:, 0], vec='w')
 
 
