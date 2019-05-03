736900132fe85e03b3953fd1a38bc6ec9553cece
saullocastro <castrosaullo@gmail.com>
2019-05-03 15:46:50 +0200

    TST: fixed tests

--- panels/tests/tests_shell/test_field_outputs.py
+++ panels/tests/tests_shell/test_field_outputs.py
@@ -1,21 +1,23 @@
 import numpy as np
 
+from structsolve import static
 from panels import Shell
 
-def test_panel_field_outputs():
+def test_panel_field_outputs(plot=False):
     m = 7
     n = 6
     for model in ['plate_clpt_donnell_bardell',
                   'cylshell_clpt_donnell_bardell']:
+        print('Testing model %s' % model)
         s = Shell()
         s.model = model
-        s.u1tx = 1
-        s.u1ty = 1
-        s.u2ty = 1
-        s.v1tx = 0
-        s.v2tx = 0
-        s.v1ty = 0
-        s.v2ty = 0
+        s.x1u = 1
+        s.y1u = 1
+        s.y2u = 1
+        s.x1v = 0
+        s.x2v = 0
+        s.y1v = 0
+        s.y2v = 0
 
         s.a = 2.
         s.b = 1.
@@ -29,16 +31,35 @@ def test_panel_field_outputs():
         s.n = n
 
         P = 1000.
-        npts = 100
-        s.point_loads_inc = []
-        for y in np.linspace(0, s.b, npts):
-            s.point_loads_inc.append([0., y, P/(npts-1.), 0, 0])
-        s.point_loads_inc[0][2] /= 2.
-        s.point_loads_inc[-1][2] /= 2.
+        Nxx = P/s.b
+        s.add_distr_load_fixed_x(0, funcx=lambda y: Nxx, funcy=None,
+                funcz=None, cte=False)
+        increments, cs = static(s.calc_kC(), s.calc_fext(), silent=True)
+        plot_mesh, fields = s.uvw(cs[0])
+        plot_mesh, fields = s.stress(cs[0])
+        _, fields = s.strain(cs[0])
+        assert np.isclose(fields.get('w').max(), 0.00014, rtol=0.05)
+        assert np.isclose(fields.get('Nxx').min(), -1011.4371924517162, rtol=0.001)
+        assert np.isclose(fields.get('eyy').min(), -4.373275882508465e-07, rtol=0.001)
 
-        s.static()
-        c = s.analysis.cs[0]
-        plot_mesh, stress = s.stress(c, gridx=50, gridy=50)
-        _, strain = s.strain(c, gridx=50, gridy=50)
+        if plot:
+            import matplotlib
+            matplotlib.use('TkAgg')
+            import matplotlib.pyplot as plt
+            x = plot_mesh['Ys']
+            y = plot_mesh['Xs']
+            field = fields['w']
+            levels = np.linspace(field.min(), field.max(), 400)
+            contour = plt.contourf(x, y, field, levels=levels)
+            cbar = plt.colorbar(contour)
+            plt.show()
+            plt.cla()
+            plt.contourf(x, y, fields['Nxx'])
+            plt.show()
+            plt.cla()
+            plt.contourf(x, y, fields['eyy'])
+            plt.show()
 
 
+if __name__ == '__main__':
+    test_panel_field_outputs(plot=True)
