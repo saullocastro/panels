35511e9348876ca6002dc8a2cb9c594f94c7e843
saullocastro <castrosaullo@gmail.com>
2019-05-03 14:34:07 +0200

    MAINT: cleaning it up

--- theory/shells/cylshell_clpt_donnell_bardell/cylshell_clpt_donnell_bardell.py
+++ theory/shells/cylshell_clpt_donnell_bardell/cylshell_clpt_donnell_bardell.py
@@ -13,11 +13,9 @@ var('Nxx_o, Nyy_o, Nxy_o, Mxx_o, Myy_o, Mxy_o')
 
 var('fAu, gAu, fAv, gAv, fAw, gAw, fAuxi, fAvxi, fAwxi, gAueta, gAveta, gAweta, fAwxixi, gAwetaeta')
 var('fBu, gBu, fBv, gBv, fBw, gBw, fBuxi, fBvxi, fBwxi, gBueta, gBveta, gBweta, fBwxixi, gBwetaeta')
-var('fCu, gCu, fCv, gCv, fCw, gCw, fCuxi, fCvxi, fCwxi, gCueta, gCveta, gCweta, fCwxixi, gCwetaeta')
 
 var('uA, vA, wA, uAxi, vAxi, wAxi, uAeta, vAeta, wAeta, wAxixi, wAxieta, wAetaeta')
 var('uB, vB, wB, uBxi, vBxi, wBxi, uBeta, vBeta, wBeta, wBxixi, wBxieta, wBetaeta')
-var('uC, vC, wC, uCxi, vCxi, wCxi, uCeta, vCeta, wCeta, wCxixi, wCxieta, wCetaeta')
 
 F = M([[A11, A12, A16, B11, B12, B16],
        [A12, A22, A26, B12, B22, B26],
@@ -58,22 +56,6 @@ swBxixi = M([[0, 0, fBwxixi*gBw]])
 swBxieta = M([[0, 0, fBwxi*gBweta]])
 swBetaeta = M([[0, 0, fBw*gBwetaeta]])
 
-suC = M([[fCu*gCu, 0, 0]])
-svC = M([[0, fCv*gCv, 0]])
-swC = M([[0, 0, fCw*gCw]])
-
-suCxi = M([[fCuxi*gCu, 0, 0]])
-svCxi = M([[0, fCvxi*gCv, 0]])
-swCxi = M([[0, 0, fCwxi*gCw]])
-
-suCeta = M([[fCu*gCueta, 0, 0]])
-svCeta = M([[0, fCv*gCveta, 0]])
-swCeta = M([[0, 0, fCw*gCweta]])
-
-swCxixi = M([[0, 0, fCwxixi*gCw]])
-swCxieta = M([[0, 0, fCwxi*gCweta]])
-swCetaeta = M([[0, 0, fCw*gCwetaeta]])
-
 GA = M([(2/a)*swAxi, (2/b)*swAeta])
 BA = M([(2/a)*suAxi + (2/a)**2*swAxi*wxi,
         (2/b)*svAeta + 1/r*swA + (2/b)**2*swAeta*weta,
@@ -81,13 +63,6 @@ BA = M([(2/a)*suAxi + (2/a)**2*swAxi*wxi,
        -(2/a)*(2/a)*swAxixi,
        -(2/b)*(2/b)*swAetaeta,
        -(2/a)*(2/b)*2*swAxieta])
-ZEROS = BA[:1, :]*0
-BA_o = M([(2/a)**2*swAxi*wxi_o,
-        (2/b)**2*swAeta*weta_o,
-        (2/a)*(2/b)*(weta_o*swAxi + wxi_o*swAeta),
-         ZEROS,
-         ZEROS,
-         ZEROS])
 B0A = BA.subs({wxi.name:0, weta.name:0})
 
 GB = M([(2/a)*swBxi, (2/b)*swBeta])
@@ -97,42 +72,13 @@ BB = M([(2/a)*suBxi + (2/a)**2*swBxi*wxi,
        -(2/a)*(2/a)*swBxixi,
        -(2/b)*(2/b)*swBetaeta,
        -(2/a)*(2/b)*2*swBxieta])
-ZEROS = BB[:1, :]*0
-BB_o = M([(2/a)**2*swBxi*wxi_o,
-        (2/b)**2*swBeta*weta_o,
-        (2/a)*(2/b)*(weta_o*swBxi + wxi_o*swBeta),
-         ZEROS,
-         ZEROS,
-         ZEROS])
 B0B = BB.subs({wxi.name:0, weta.name:0})
-
-GC = M([(2/a)*swCxi, (2/b)*swCeta])
-BC = M([(2/a)*suCxi + (2/a)**2*swCxi*wxi,
-        (2/b)*svCeta + 1/r*swC + (2/b)**2*swCeta*weta,
-        (2/b)*suCeta + (2/a)*svCxi + (2/a)*(2/b)*(weta*swCxi + wxi*swCeta),
-       -(2/a)*(2/a)*swCxixi,
-       -(2/b)*(2/b)*swCetaeta,
-       -(2/a)*(2/b)*2*swCxieta])
-ZEROS = BC[:1, :]*0
-BC_o = M([(2/a)**2*swCxi*wxi_o,
-        (2/b)**2*swCeta*weta_o,
-        (2/a)*(2/b)*(weta_o*swCxi + wxi_o*swCeta),
-         ZEROS,
-         ZEROS,
-         ZEROS])
-B0C = BC.subs({wxi.name:0, weta.name:0})
-
 
 # Constitutive stiffness matrix
 kC = intx*inty/4*BA.T*F*BB
 
 # Internal force vector
 fint = intx*inty/4*BA.T*M([[Nxx, Nyy, Nxy, Mxx, Myy, Mxy]]).T
-
-# Higher order tensors required by displacement based Ritz-Koiter
-eAB1 = (2/a)**2*swAxi.T*swBxi
-eAB2 = (2/b)**2*swAeta.T*swBeta
-eAB3 = (2/a)*(2/b)*(swAxi.T*swBeta + swAeta.T*swBxi)
 
 # Linear constitutive stiffness matrix
 
@@ -180,6 +126,7 @@ vars = (A11, A12, A16, A22, A26, A66,
 piece_wise_simplify(kC, vars)
 piece_wise_simplify(kC0, vars)
 piece_wise_simplify(kG, (Nxx, Nyy, Nxy))
+piece_wise_simplify(fint, vars)
 
 from panels.dev.matrixtools import mprint_as_sparse
 
@@ -196,10 +143,7 @@ matrices = [
     [kAx, 'kAx'],
     [kAy, 'kAy'],
     [cA, 'cA'],
-    [BA, 'eAkA'],
-    [eAB1, 'eAB1'],
-    [eAB2, 'eAB2'],
-    [eAB3, 'eAB3'],
+    [fint, 'fint'],
 ]
 
 for m in matrices:
