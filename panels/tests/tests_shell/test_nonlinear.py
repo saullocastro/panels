098ac2419310a1329ab10fdfd65ae0c1bd215748
saullocastro <castrosaullo@gmail.com>
2019-05-03 14:59:41 +0200

    TST: BUG fixed based on new structsolve interface

--- panels/tests/tests_shell/test_nonlinear.py
+++ panels/tests/tests_shell/test_nonlinear.py
@@ -1,52 +1,53 @@
+import sys
+sys.path.append(r'C:\repositories\structsolve')
 from copy import deepcopy
 
-import matplotlib.pyplot as plt
 import numpy as np
 
+from structsolve import Analysis
 from panels import Shell
 
 
-def test_nonlinear():
+def test_nonlinear(plot=False):
     m = 6
     n = 6
-    for model in ['plate_clpt_donnell_bardell',
-                  'cylshell_clpt_donnell_bardell',
+    for model in [
+            'plate_clpt_donnell_bardell',
+            'cylshell_clpt_donnell_bardell',
                   ]:
+        print('Testing model: %s' % model)
         s = Shell()
+        an = Analysis(calc_fext=s.calc_fext,
+                      calc_fint=s.calc_fint,
+                      calc_kC=s.calc_kC,
+                      calc_kG=s.calc_kG)
+
         s.model = model
-        s.u1tx = 1
-        s.u1rx = 1
-        s.u2tx = 0
-        s.u2rx = 1
+        s.x1u = 1
+        s.x2u = 0
 
-        s.v1tx = 1
-        s.v1rx = 1
-        s.v2tx = 1
-        s.v2rx = 1
+        s.x1v = 1
+        s.x2v = 1
 
-        s.w1tx = 0
-        s.w1rx = 1
-        s.w2tx = 0
-        s.w2rx = 1
+        s.x1w = 0
+        s.x1wr = 1
+        s.x2w = 0
+        s.x2wr = 1
 
-        s.u1ty = 1
-        s.u1ry = 1
-        s.u2ty = 1
-        s.u2ry = 1
+        s.y1u = 1
+        s.y2u = 1
 
-        s.v1ty = 0
-        s.v1ry = 1
-        s.v2ty = 1
-        s.v2ry = 1
+        s.y1v = 0
+        s.y2v = 1
 
-        s.w1ty = 0
-        s.w1ry = 1
-        s.w2ty = 0
-        s.w2ry = 1
+        s.y1w = 0
+        s.y1wr = 1
+        s.y2w = 0
+        s.y1wr = 1
 
         s.a = 4.
         s.b = 1.
-        s.r = 1.e5
+        s.r = 1.e15
         s.stack = [90, 0, 90, 0]
         s.plyt = 1e-3*0.125
         E11 = 142.5e9
@@ -58,33 +59,40 @@ def test_nonlinear():
         s.m = m
         s.n = n
 
-        P = 25
+        P = 500
         Nxx = P/s.b
-        npts = 10000
-        s.point_loads_inc = []
-        for y in np.linspace(0, s.b, npts):
-            s.point_loads_inc.append([0., y, P/(npts-1.), 0, 0])
-        s.point_loads_inc[0][2] /= 2.
-        s.point_loads_inc[-1][2] /= 2.
+        # distributed axial load
+        s.add_distr_load_fixed_x(0, funcx=None, funcy=lambda y: Nxx,
+                funcz=None, cte=False)
+        # perturbation load
         s.point_loads.append([s.a/2., s.b/2., 0, 0, 0.001])
 
-        s.analysis.maxArcLength = 8
-        s.analysis.NL_method = 'arc_length_riks'
-        s.analysis.modified_NR = True
-        s.static(NLgeom=True, silent=False)
-        analysis_arc_length = deepcopy(s.analysis)
-        assert np.isclose(s.analysis.increments[-1], 0.5175975949998647, rtol=0.01)
+        an.NL_method = 'NR'
+        an.modified_NR = True
+        an.static(NLgeom=True, silent=True)
+        analysis_newton_raphson = deepcopy(an)
+        print('    Newton-Raphson', an.increments[-1])
+        assert np.isclose(an.increments[-1], 0.545674321, rtol=0.01)
 
+        an.maxArcLength = 8
+        an.NL_method = 'arc_length_riks'
+        an.modified_NR = True
+        an.static(NLgeom=True, silent=True)
+        analysis_arc_length = deepcopy(an)
+        print('    Arc-Length', an.increments[-1])
+        assert np.isclose(an.increments[-1], 0.27193678, rtol=0.01)
 
-        s.analysis.NL_method = 'NR'
-        s.analysis.modified_NR = True
-        s.static(NLgeom=True, silent=False)
-        analysis_newton_raphson = deepcopy(s.analysis)
-        assert np.isclose(s.analysis.increments[-1], 0.545674321, rtol=0.01)
+        if plot:
+            import matplotlib
+            matplotlib.use('TkAgg')
+            import matplotlib.pyplot as plt
+            wmaxs = [s.uvw(c)[1]['w'].max() for c in analysis_newton_raphson.cs]
+            plt.plot(wmaxs, analysis_newton_raphson.increments, label='NR')
+            wmaxs = [s.uvw(c)[1]['w'].max() for c in analysis_arc_length.cs]
+            plt.plot(wmaxs, analysis_arc_length.increments, '--',
+                    label='Arc-Length')
+            plt.legend()
+            plt.show()
 
-        wmaxs = [s.uvw(c)[1]['w'].max() for c in analysis_newton_raphson.cs]
-        plt.plot(wmaxs, analysis_newton_raphson.increments)
-
-        wmaxs = [s.uvw(c)[1]['w'].max() for c in analysis_arc_length.cs]
-        plt.plot(wmaxs, analysis_arc_length.increments, '--')
-
+if __name__ == '__main__':
+    test_nonlinear(plot=True)
