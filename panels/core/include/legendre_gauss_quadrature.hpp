896583c0f93abe94b68f627afe9ee2c5568f9149
Saullo G. P. Castro <saullogiovani@gmail.com>
2025-07-31 09:02:50 +0200

    MAINT: separating the n<=30 from the n<=304 Legendre-Gauss, this should reduce compilation time and size of some compiled files

--- panels/core/include/legendre_gauss_quadrature.hpp
+++ panels/core/include/legendre_gauss_quadrature.hpp
@@ -1,4 +1,5 @@
 #ifndef LEGENDRE_GAUSS_QUADRATURE_H
 #define LEGENDRE_GAUSS_QUADRATURE_H
 void leggauss_quad(int n, double *points, double *weights);
+void leggauss_quad_304(int n, double *points, double *weights);
 #endif /** LEGENDRE_GAUSS_QUADRATURE_H */
