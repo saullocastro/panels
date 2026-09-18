6da8a7b733877eef2bded20fbfd49c0d1c289721
saullocastro <saullogiovani@gmail.com>
2026-09-18 11:41:55 +0200

    MAINT: remove Python 3.8

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ Topic :: Software Development :: Libraries :: Python Modules
 Operating System :: Microsoft :: Windows
 Operating System :: Unix
 Operating System :: POSIX :: BSD
-Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3.11
